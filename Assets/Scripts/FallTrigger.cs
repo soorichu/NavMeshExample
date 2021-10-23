46e7b16c864a726c3862ae4c1467a03cfd58547c
soorichu <soorichu@gmail.com>
2021-10-23 16:12:46 +0900

    베르

---
+++ Assets/Scripts/FallTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallTrigger : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody fallBody;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // 트리거가 충돌을 감지하면 장애물에 중력을 적용해서 떨어지게 만든다
+        fallBody.useGravity = true;
+    }
+}
